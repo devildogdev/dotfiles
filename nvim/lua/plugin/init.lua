17f5e777e30a833f82702ffadd65ecc9842c5866
j-tew <justinrtew@gmail.com>
2023-11-26 22:51:49 -0600

    Fix treesitter and git prompt

--- nvim/lua/plugin/init.lua
+++ nvim/lua/plugin/init.lua
@@ -126,22 +126,29 @@ require('lazy').setup({
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    opts = {
-      ensure_installed = {
-        'c',
-        'lua',
-        'vim',
-        'vimdoc',
-        'query',
-        'go',
-        'python',
-        'html',
-        'css',
-        'javascript',
-        'json',
-      },
-    },
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = {
+            'c',
+            'lua',
+            'vim',
+            'vimdoc',
+            'json',
+            'query',
+            'go',
+            'python',
+            'javascript',
+            'html',
+            'css',
+            'bash'
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end,
   },
 
 -- STATUSLINE
