1f2bce794ed7db676c0461ba037a7bae5437949b
devildogdev <justinrtew@gmail.com>
2024-09-01 22:48:58 -0500

    switch to zed

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,54 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require('options')
 require('keymaps')
-require('lazy').setup('plugins', {})
+require('lazy').setup({
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "catppuccin-mocha"
+        end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = {
+                    'c',
+                    'lua',
+                    'vim',
+                    'vimdoc',
+                    'json',
+                    'query',
+                    'go',
+                    'python',
+                    'javascript',
+                    'html',
+                    'css',
+                    'bash'
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+}, {})
