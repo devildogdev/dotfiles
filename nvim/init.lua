230c57ccbc1e65bbc43dabffaa87b4ce6a9f78bc
devildogdev <justinrtew@gmail.com>
2025-04-13 19:42:21 -0500

    I still wasn't happy...

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,8 +23,31 @@ vim.g.loaded_ruby_provider = false
 vim.g.loaded_python3_provider = false
 vim.g.loaded_node_provider = false
 
-require('options')
-require('keymaps')
+vim.o.completeopt = 'menuone,noselect'
+vim.o.expandtab = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.mouse = 'nv'
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ruler = false
+vim.o.scrolloff = 10
+vim.o.shiftwidth = 4
+vim.o.signcolumn = 'yes'
+vim.o.smartcase = true
+vim.o.softtabstop = 4
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.wrap = false
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n' }, '<Leader>ef', ':Explore<CR>')
+vim.keymap.set({ 'n', 'v' }, "<leader>y", [["+y]])
+vim.keymap.set({ 'n', 'v' }, "<leader>yy", [["+yy]])
+vim.keymap.set({ 'n' }, '<leader>Y', [["+Y]])
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 require('lazy').setup({
   spec = {
     { import = 'plugins' }
--- nvim/lua/keymaps.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n' }, '<Leader>ef', ':Explore<CR>')
-vim.keymap.set({ 'n', 'v' }, "<leader>y", [["+y]])
-vim.keymap.set({ 'n', 'v' }, "<leader>yy", [["+yy]])
-vim.keymap.set({ 'n' }, '<leader>Y', [["+Y]])
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
